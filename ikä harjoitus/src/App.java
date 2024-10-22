bfe37865055a33bc6246c8790b696afb8367faf3
Laura S. <lsaarinen85@gmail.com>
2024-10-22 14:31:57 +0300

    Kirjoitin moi

---
+++ ikä harjoitus/src/App.java
@@ -0,0 +1,34 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+
+      int ika = 20;
+      // tulostusehdot
+      if (ika >= 0 && ika < 18)
+      {
+      System.out.println("Olet alaikäinen");
+      if (ika >= 15); {
+        System.out.println("Saat ajaa mopoa");
+      }
+      if (ika >= 16 )
+      {
+        System.out.println("Saat ajaa kevaria");
+       }
+      }
+      else if (ika >= 65)
+      {
+      System.out.println("Olet eläkeläinen");
+      }
+      else
+      {
+        System.out.println("Olet aikuinen");
+      }
+       
+      if (ika == 18) { 
+        System.out.println("Olet täysi-ikäinen, saat ajaa autoa");
+      } moi;
+       
+      
+       
+    }
+}
